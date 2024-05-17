42659ed8360766500b1395f66a9f7511ebec3f40
HalosDev <halos@halosdev.com>
2024-05-17 16:17:02 -0400

    Permission Command

--- src/main/java/com/halosdev/halosfirstplugin/Main.java
+++ src/main/java/com/halosdev/halosfirstplugin/Main.java
@@ -10,17 +10,21 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        // Config Stuff
         getConfig().options().copyDefaults();
         saveDefaultConfig();
+        // Startup Out Put
         System.out.println("Halos First Plugin has enabled.");
+        // Events register
         Bukkit.getPluginManager().registerEvents(new MoveEvent(), this);
         Bukkit.getPluginManager().registerEvents(new EggThrow(), this);
+        // Command Registers
         getCommand("heal").setExecutor(new Heal());
         getCommand("hello").setExecutor(new Hello());
         getCommand("console").setExecutor(new ConsoleCommand());
         getCommand("config").setExecutor(new ConfigCommand(this));
         getCommand("word").setExecutor(new SetWord(this));
+        getCommand("permission").setExecutor(new Permission());
 
     }
 
--- src/main/java/com/halosdev/halosfirstplugin/commands/Permission.java
+++ src/main/java/com/halosdev/halosfirstplugin/commands/Permission.java
@@ -1,18 +1,24 @@
 package com.halosdev.halosfirstplugin.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-public class TP implements CommandExecutor {
+public class Permission implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
 
         if (commandSender instanceof Player) {
             Player player = (Player) commandSender;
 
+            if (player.hasPermission("halosplugin.use")){
+                player.sendMessage(ChatColor.GREEN + "You have permission to use this");
+            } else {
+                player.sendMessage(ChatColor.RED + "You do not have persmison to use this command.");
+            }
         }
 
         return false;
